e1930d3994ee16c178f32e6aae1dd9a8106efc3e
HamiLia <cvsd1144531@gmail.com>
2026-01-23 21:54:14 -0500

    lab wk 02

--- Lab01_101503901_COMP3133/index.js
+++ Lab01_101503901_COMP3133/index.js
@@ -1,26 +1,46 @@
 const fs = require("fs");
 const path = require("path");
+const csv = require("csv-parser");
 
-// Define the file path
-const filePath = path.join(__dirname, "input.txt");
+const inputFilePath = path.join(__dirname, "input_countries.csv");
+const canadaFilePath = path.join(__dirname, "canada.txt");
+const usaFilePath = path.join(__dirname, "usa.txt");
 
-console.log("--- START ---");
-
-// Read synchronously
-try {
-  const data = fs.readFileSync(filePath, "utf8");
-  console.log(data);
-} catch (err) {
-  console.error("Error reading file:", err);
+// 기존 파일 삭제
+if (fs.existsSync(canadaFilePath)) {
+  fs.unlinkSync(canadaFilePath);
 }
 
-// Read asynchronously
-fs.readFile(filePath, "utf8", (err, data) => {
-  if (err) {
-    console.error("Error reading file:", err);
-    return;
-  }
-  console.log(data);
-});
+if (fs.existsSync(usaFilePath)) {
+  fs.unlinkSync(usaFilePath);
+}
 
-console.log("--- END ---");
+// 헤더 작성
+fs.writeFileSync(canadaFilePath, "country,year,population\n");
+fs.writeFileSync(usaFilePath, "country,year,population\n");
+
+console.log("---START---");
+
+fs.createReadStream(inputFilePath)
+  .pipe(csv())
+  .on("data", (row) => {
+    const country = row.country.toLowerCase();
+
+    if (country === "canada") {
+      fs.appendFileSync(
+        canadaFilePath,
+        `${row.country},${row.year},${row.population}\n`,
+      );
+    }
+
+    if (country === "united states") {
+      fs.appendFileSync(
+        usaFilePath,
+        `${row.country},${row.year},${row.population}\n`,
+      );
+    }
+  })
+  .on("end", () => {
+    console.log("CSV file successfully processed");
+    console.log("---END---");
+  });
