349a0798429aea5832c5620fc18f7cb62c3d55f9
HamiLia <cvsd1144531@gmail.com>
2026-01-23 19:22:02 -0500

    add some files

--- Lab01_101503901_COMP3133/readStreamExample.js
+++ Lab01_101503901_COMP3133/readStreamExample.js
@@ -3,15 +3,19 @@ const path = require("path");
 
 // Define the file path
 const filePath = path.join(__dirname, "input_stream.txt");
-const readStream = fs.createReadStream(filePath);
-readStream.on("data", (chunk) => {
-  console.log("Received chunk:");
-  console.log(chunk.toString());
+const readSteam = fs.createReadStream(filePath);
+
+readSteam.on("data", (chunk) => {
+  console.log("Received chunk:", chunk.toString());
 });
 
-readStream.on("end", () => {
+readSteam.on("end", () => {
   console.log("No more data to read.");
 });
-readStream.on("error", (err) => {
+
+readSteam.on("error", (err) => {
   console.error("Error reading the stream:", err);
 });
+
+// readStream.pause();
+//vreadStream.resume();
